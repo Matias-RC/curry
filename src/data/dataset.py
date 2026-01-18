0676416b27e8256b4c868cb8ab207b8a23189207
furrutiav <furrutia@dim.uchile.cl>
2026-01-18 19:35:53 -0300

    dataset improved

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,10 @@
 from torch.utils.data import Dataset
 import torch
 from tqdm import tqdm
+from datasets import load_dataset, Features, Value
+from typing import Set, Tuple
+from pathlib import Path
+import requests
 
 import pandas as pd
 import numpy as np
@@ -8,20 +12,41 @@ import os
 
 from collections import deque
 
-def load_level_by_id(path: str, level_id: str) -> str:
-    """
-    path: path to the .txt level file
-    level_id: stringified triple, e.g. "000", "014"
+def load_level_by_id(config_dataset, folder_name: str, level_name: str) -> str:
+    repo = config_dataset["source_levels"]["github"]
+    split = config_dataset['split']
+    split = split if split != "validation" else "valid"
 
-    Returns:
-        Level grid as a single string (with newlines)
-    """
-    target = int(level_id)  # "000" -> 0
+    split_path = f"{config_dataset['difficulty']}/{split}"
+    cache_dir = Path(config_dataset["source_levels"]["cache_dir"]).expanduser()
+    root_path = cache_dir / Path(repo) / Path(split_path)
+    if not root_path.exists():
+        root_path.mkdir(parents=True, exist_ok=True)
+        api_url = f"https://api.github.com/repos/{repo}/contents/{split_path}?ref=master"
+
+        response = requests.get(api_url)
+        response.raise_for_status()
+
+        files = response.json()
+
+        unexisting_files = [file for file in files if file["type"] == "file" and file["name"].endswith(".txt") and not (root_path / file["name"]).exists()]
+        tqdm_iterator = tqdm(unexisting_files, desc="Downloading files", unit="file")
+        for file in tqdm_iterator:
+            download_url = file["download_url"]
+            filename = root_path / file["name"]
+
+            r = requests.get(download_url)
+            r.raise_for_status()
+
+            filename.write_bytes(r.content)
+
+    target = int(level_name)  # "000" -> 0
     current_id = None
     collecting = False
     level_lines = []
-
-    with open(path, "r") as f:
+    folder_name = str(folder_name).zfill(3)  # 0 - > 000
+    file_path = root_path / Path(f"{folder_name}.txt")
+    with open(file_path, "r") as f:
         for line in f:
             line = line.rstrip("\n")
 
@@ -45,16 +70,11 @@ def load_level_by_id(path: str, level_id: str) -> str:
                 level_lines.append(line)
 
     if not level_lines:
-        raise ValueError(f"Level {level_id} not found in file")
+        raise ValueError(f"Level {level_name} not found in file")
 
     return "\n".join(level_lines)
 
-def fill_name(string, length=3):
-    while len(string) < length:
-        string = "0" + string
-    return string
 
-from typing import Set, Tuple
 
 def parse_sokoban_level(level_str: str):
     """
@@ -106,25 +126,20 @@ def parse_sokoban_level(level_str: str):
     }   
 
 def play(state, actions_str):
-    is_valid_play = True
+    status = "incomplete"
     action_map = [(-1,0),(0,1),(1,0),(0,-1)]
     states = [state]
     for action in actions_str:
         walls, boxes, goals, player = state["walls"], state["boxes"], state["goals"], state["player"]
-        if tuple(sorted(boxes)) == tuple(sorted(goals)):
-            print("solved")
-            break
         dy, dx = action_map[int(action)]
         new_pos_player = (player[0]+dy, player[1]+dx)
         if new_pos_player in walls:
-            is_valid_play = False
-            print("hit wall")
+            status = "hit wall"
             break
         if new_pos_player in boxes:
             new_pos_box = (new_pos_player[0]+dy, new_pos_player[1]+dx)
             if new_pos_box in boxes:
-                is_valid_play = False
-                print("box hit box")
+                status = "box hit box"
                 break
             boxes.remove(new_pos_player)
             boxes.add(new_pos_box)
@@ -133,7 +148,10 @@ def play(state, actions_str):
         state = {"walls": walls, "boxes": boxes, "goals": goals, "player": player}
         states.append(state)
 
-    return states, is_valid_play
+    if tuple(sorted(boxes)) == tuple(sorted(goals)):
+        status = "solved"
+    
+    return states, status
 
 def symbolic_state_to_tensor(state, grid_shape_x, grid_shape_y, channels):
     num_channels = len(channels)
@@ -146,64 +164,92 @@ def symbolic_state_to_tensor(state, grid_shape_x, grid_shape_y, channels):
 
     return tensor
 
+def get_solution_subset(config_dataset):
+    source_solutions = load_dataset(
+            config_dataset["source_solutions"]["huggingface"],
+            cache_dir=config_dataset["source_solutions"]["cache_dir"],
+            split=config_dataset["split"],
+            features=Features({
+                "File": Value("int64"),
+                "Level": Value("int64"),
+                "Actions": Value("string"),
+                "Steps": Value("string"),
+                "SearchSteps": Value("int64"),
+            })
+        )
+    
+    source_solutions = source_solutions.filter(
+            lambda example: example["Steps"] != "INCORRECT_SOLUTION_FOUND" and
+                            example["Actions"] != "SEARCH_STATE_FAILED" and
+                            example["Actions"] != "NOT_FOUND"
+        )
+        
+    source_solutions = source_solutions.map(lambda example: {"Steps": int(example["Steps"])})
+    source_solutions = source_solutions.filter(
+        lambda example: example["Steps"] == len(example["Actions"])
+    )
+
+    return source_solutions
+
 class SokobanDataset(Dataset):
     def __init__(self, config):
 
-        self.config = config
         self.data = self.load_data(config)
 
 
     def load_data(self, config):
 
-        difficulty = config["difficulty"]
-        subset_name = config["subset_name"]
-
-        grid_shape_x = config["grid_shape_x"]
-        grid_shape_y = config["grid_shape_y"]
+        grid_shape_x = config["grid_shape"]["x"]
+        grid_shape_y = config["grid_shape"]["y"]
 
         max_num_levels = config["max_num_levels"]
-        filter_by = config.get("filter_by", "no_filter")
+        order_by = config["order_by"]  # "shortest_first", "longest_first", "no_filter"
 
         channels = ['boxes', 'goals', 'player', 'walls']
+        
+        source_solutions = get_solution_subset(config)
 
-        df = pd.read_csv(f"../../boxoban-astar-solutions/{difficulty}_{subset_name}.csv")
-        filtered = df[
-            (df["Steps"] != "INCORRECT_SOLUTION_FOUND") &
-            (df["Actions"] != "SEARCH_STATE_FAILED") &
-            (df["Actions"] != "NOT_FOUND") &
-            (df["Steps"] != -1)
-        ].copy()
-
-        # Convert Steps to numeric
-        filtered["Steps"] = pd.to_numeric(filtered["Steps"], errors="coerce")
-        filtered = filtered.dropna(subset=["Steps"])
-
+        min_length = config["solution_lenght"]["min"]
+        max_length = config["solution_lenght"]["max"]
+        filtered = source_solutions.filter(
+                lambda example: (example["Steps"] >= min_length) and (example["Steps"] <= max_length)
+            )
         # Sort deterministically
-        if filter_by == "shortest_first":
-            filtered = filtered.sort_values("Steps", ascending=True)
-        elif filter_by == "longest_first":
-            filtered = filtered.sort_values("Steps", ascending=False)
-        # else: no_filter → keep CSV order
+        if order_by == "shortest_first":
+            filtered = filtered.sort("Steps", reverse=False)
+        elif order_by == "longest_first":
+            filtered = filtered.sort("Steps", reverse=True)
+        elif order_by == "shuffle":
+            filtered = filtered.shuffle(seed=config["seed"])
+        elif order_by == "no_filter":
+            pass
+        else:
+            raise ValueError(f"Unknown order_by: {order_by}")
 
         data = []
         count_levels = 0
         ix = 0
+        stats_levels = {"solved": {"boxes": [], "sol_len": [], "walls": []}, "not solved": {}}
         while (count_levels < max_num_levels) and (ix < len(filtered)):
-            row = filtered.iloc[ix]
-            folder_name = fill_name(str(int(row["File"])))
-            level_id_filled = fill_name(str(int(row["Level"])))
+            row = filtered[ix]
+            folder_name = row["File"]
+            level_name = row["Level"]
 
             level = load_level_by_id(
-                f"../../boxoban-levels/{difficulty}/{subset_name}/{folder_name}.txt",
-                level_id_filled
+                config,
+                folder_name,
+                level_name,
             )
+            total_boxes = level.count("$") + level.count("*")
+            total_walls = level.count("#")
 
             actions_str = row["Actions"]
+            solution_length = len(actions_str)
 
             state_0 = parse_sokoban_level(level)
-            states, is_valid_play = play(state_0, actions_str) 
+            states, status = play(state_0, actions_str) 
 
-            if is_valid_play:
+            if status == "solved":
                 states_tensor = [
                     symbolic_state_to_tensor(state, grid_shape_x, grid_shape_y, channels)
                     for state in states[:-1]
@@ -213,15 +259,28 @@ class SokobanDataset(Dataset):
                 actions_id = torch.tensor([int(a) for a in actions_str], dtype=torch.long)
 
                 data.append({
+                    "level_str": level,
+                    "actions_str": actions_str,
+                    "folder_name": folder_name,
+                    "level_name": level_name,
                     "states_tensor": states_tensor,
                     "actions_id": actions_id,
                 })
                 count_levels += 1
+
+                stats_levels[status]["boxes"].append(total_boxes)
+                stats_levels[status]["sol_len"].append(solution_length)
+                stats_levels[status]["walls"].append(total_walls)
                 
             else:
-                print(f"Skipping level {level_id_filled} in folder {folder_name} due to invalid playthrough.")
+                if status not in stats_levels["not solved"]:
+                    stats_levels["not solved"][status] = 0
+                stats_levels["not solved"][status] += 1
 
             ix += 1
+        
+        print(pd.DataFrame(stats_levels["solved"]).describe())
+        print("Not solved stats:", stats_levels["not solved"])
 
         return data
     
@@ -240,7 +299,16 @@ def collate_fn(batch):
     batch_states_padded = torch.nn.utils.rnn.pad_sequence(batch_states, batch_first=True, padding_value=0.0)
     batch_actions_padded = torch.nn.utils.rnn.pad_sequence(batch_actions, batch_first=True, padding_value=-100)
 
+    level_strs = [item["level_str"] for item in batch]
+    action_strs = [item["actions_str"] for item in batch]
+    folder_names = [item["folder_name"] for item in batch]
+    level_names = [item["level_name"] for item in batch]
+
     return {
+        "level_strs": level_strs,
+        "action_strs": action_strs,
+        "folder_names": folder_names,
+        "level_names": level_names,
         "states_tensors": batch_states_padded,
         "actions_ids": batch_actions_padded
     }
@@ -249,46 +317,59 @@ def collate_fn(batch):
 # Example usage
 if __name__ == "__main__":
     config_dataset = {
-        "difficulty": "medium",
-        "subset_name": "valid",
-        "grid_shape_x": 10,
-        "grid_shape_y": 10,
-        "max_num_levels": 10,
-        "filter_by": "shortest_first",  # Options: "shortest_first", "longest_first", "no_filter"
-    }
+        "source_levels": {
+            "github": "google-deepmind/boxoban-levels",
+            "cache_dir": "~/scratch/curry/",
+        },
+        "source_solutions": {
+            "huggingface": "AlignmentResearch/boxoban-astar-solutions",
+            "cache_dir": "~/scratch/curry/",
+        },
 
+        "difficulty": "unfiltered",
+        "split": "validation",
+        "grid_shape": {
+            "x": 10,
+            "y": 10
+        },
+        "max_num_levels": 16,
+        "solution_lenght":{
+            "min": 1,
+            "max": 50,
+        },
+        "order_by": "shortest_first",
+    }
     dataset = SokobanDataset(config_dataset)
     print(next(iter(dataset)))
-    print(len(dataset[0]["actions_id"]))
 
-    """{'states_tensor': tensor([[[[0., 0., 0., 1.],
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.],
-           ...,
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.]],
- 
-          [[0., 0., 0., 1.],
-           [0., 0., 0., 0.],
-           [0., 0., 0., 0.],
-           ...,
-           [0., 0., 0., 0.],
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.]],
- 
-          [[0., 0., 0., 1.],
-           [0., 0., 0., 0.],
-           [0., 0., 0., 0.],
-           ...,
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.]],
- 
-          ...,
-...
-           [0., 0., 0., 1.],
-           [0., 0., 0., 1.]]]]),
- 'actions_id': tensor([3, 2, 3, 0, 2, 3, 3, 0, 0, 0, 3, 3, 0, 0, 0, 1, 2, 3, 2, 2, 1, 1, 2, 2,
-         2, 3, 3, 0, 0, 1, 0, 1, 2, 2, 0, 3, 3, 0, 0, 0, 1, 0, 0, 3, 2, 1, 1, 1,
-         1, 0, 1, 2, 2, 1, 2, 2, 3, 0, 2, 2, 2, 3, 2, 3, 3])}"""
+    """{'level_str': '##########\n##########\n##########\n##########\n##@#######\n# $   ####\n# ..$$ ###\n#  $.  ###\n#. #    ##\n##########\n',
+    'actions_str': '211123233112110303033322',
+    'folder_name': 0,
+    'level_name': 2,
+    'states_tensor': tensor([[[[0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            ...,
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.]],
+    
+            [[0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            ...,
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.]],
+    
+            [[0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            ...,
+            [0., 0., 0., 1.],
+    ...
+            ...,
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.],
+            [0., 0., 0., 1.]]]]),
+    'actions_id': tensor([2, 1, 1, 1, 2, 3, 2, 3, 3, 1, 1, 2, 1, 1, 0, 3, 0, 3, 0, 3, 3, 3, 2..."""
