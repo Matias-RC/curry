0d81fbb65170ef05b1c1451b05653cefcee8c069
Matias-RC <matias.rodriguez@cenia.cl>
2026-03-12 15:02:40 -0300

    Updates

--- XtraBuffer.py
+++ XtraBuffer.py
@@ -16,6 +16,24 @@ from stable_baselines3.common.buffers import RolloutBuffer, BaseBuffer
 from stable_baselines3.common.type_aliases import RolloutBufferSamples
 from stable_baselines3.common.type_aliases import GymEnv, Schedule, MaybeCallback, Any
 
+from dataclasses import dataclass
+
+@dataclass
+class RlSamples(NamedTuple):
+    observations: th.Tensor
+    actions: th.Tensor
+    old_values: th.Tensor
+    old_log_prob: th.Tensor
+    advantages: th.Tensor
+    prefixes: th.Tensor
+
+@dataclass
+class SupervisionSamples(NamedTuple):
+    inputs: th.Tensor # shape = (B, num_envs, T, *obs_shape)
+    mask: th.Tensor # shape = inputs.shape
+    targets: th.Tensor # shape = (num_envs, prefix_shape) -> (B, ...)
+
+
 class SinglePrefixBuffer(RolloutBuffer):
     observations: np.ndarray
     actions: np.ndarray
@@ -25,6 +43,8 @@ class SinglePrefixBuffer(RolloutBuffer):
     episode_starts: np.ndarray
     log_probs: np.ndarray
     values: np.ndarray
+    optimizer_class: Type[th.optim.SGD]
+    prefixes = Type[nn.Parameter]
     def __init__(
             self,
             buffer_size: int,
@@ -38,7 +58,6 @@ class SinglePrefixBuffer(RolloutBuffer):
             gamma: float = 0.99,
             n_envs: int = 1,
     ):
-        super().__init__(buffer_size, observation_space, action_space, device, gae_lambda, gamma, n_envs)
         if prefix_optimizer != th.optim.SGD:
             # Warn the user
             warnings.warn("Using optimizers other than SGD may lead to unexpected behavior.\
@@ -47,4 +66,30 @@ class SinglePrefixBuffer(RolloutBuffer):
                            momentum or Adam.")
             
         self.prefix_shape = prefix_shape
-        
+        self.prefix_learning_rate = prefix_lr
+        self.optimizer_class = prefix_optimizer
+        self.prefixes = None
+        super().__init__(
+            buffer_size,
+            observation_space,
+            action_space,
+            device,
+            gae_lambda,
+            gamma,
+            n_envs
+        )
+    
+    def simple_reset(self) -> None:
+        super().reset()
+    
+    def hard_reset(self) -> None:
+        super().reset()
+        self.prefixes = self.prefixes.detach().clone()
+    
+    def set_prefix(self, prefixes) -> Tuple[Type[nn.Module], Type[nn.Module]]:
+        """
+        No longer using Ids to verify the position of a prefix.
+        """
+        self.prefixes = nn.Parameter(prefixes)
+        optimizer = self.optimizer_class(self.prefixes, lr=self.prefix_learning_rate)
+        return self.prefixes, optimizer #Should be deleated later
--- XtraPolicy.py
+++ XtraPolicy.py
@@ -1,3 +1,7 @@
 """
 The Policy has a initial memory that is parametric that tries to acomodate itself to the necesities of all envs rest is trivial
 """
+from stable_baselines3.common.policies import ActorCriticPolicy
+
+class FullAtt:
+    pass
